2a752724f512d1d83fb25d3142935449889d14ab
Dominic Dewhurst <ddewhurs@oswego.edu>
2017-03-06 22:43:54 -0500

    Add files via upload

---
+++ cachesimulator.c
@@ -0,0 +1,196 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+typedef struct{
+	long tag;
+	int valid;
+	int counter;
+	int age;
+	int id;
+} line;
+
+line **cache;
+int missPen = 0;
+int hitRate = 0;
+int *pmissPen = &missPen;
+int *phitRate = &hitRate;
+unsigned long tag_mask;
+unsigned long set_mask;
+unsigned long *ptag_mask = &tag_mask;
+unsigned long *pset_mask = &set_mask;
+unsigned long zero_comp = ~0;
+
+int is_full(int S, int E){
+	for (int i = 0; i < S; i++){
+		for (int j = 0; j < E; j++){
+			if(cache[i][j].valid == 0){
+				return 0;
+			}
+		}
+	}
+	return 1;
+}
+
+
+
+void initialize_cache(int S, int E){
+	int tmp = 0;
+	for (int i = 0; i < S; i++){
+		for (int j = 0; j < E; j++){
+			cache[i][j].tag = 0;
+			cache[i][j].valid = 0;
+			cache[i][j].counter = 0;
+			cache[i][j].age = 0;
+			cache[i][j].id = tmp;
+			tmp++;
+		}
+	}
+}
+
+void inc_age(int S, int E, int setIndex, int tmp){
+	for (int i = 0; i < S; i++){
+		for (int j = 0; j < E; j++){
+			if (cache[i][j].id != cache[setIndex][tmp].id){
+				cache[i][j].age++;
+				}
+			}
+		}
+	}
+
+
+int replace(char policy[], int set, int E){
+	int lineIndex = 0;
+	if (strcmp(policy,"LFU") == 0){
+			int lfu = 10000;
+			for (int i = 0; i < E; i++){
+				if (cache[set][i].counter < lfu){
+					lfu = cache[set][i].counter;
+					lineIndex = i;
+				}
+			}
+			return lineIndex;
+		}
+	 else if (strcmp(policy,"LRU") ==0){
+		int lru = 0;
+		for (int i = 0; i < E; i++){
+			if (cache[set][i].age > lru){
+				lru = cache[set][i].age;
+				lineIndex = i;
+			}
+		}
+		return lineIndex;
+	} else {
+		printf("You did not enter a valid replacement policy\n");
+		return -1;
+	}
+}
+
+int check_hit(long address, line* l){
+	//printf("line: %d valid: %d tag: %ld\n",l->id, l->valid, l->tag);
+	int ret_value = (l->valid && (l->tag == (address & tag_mask)));
+	if(ret_value){
+		l->counter++;
+	}
+	return ret_value;
+}
+
+
+int main(){
+
+	int S, E, B, m, hitTime, missPenalty;
+	char policy[4];
+	//printf("Enter the number of sets:\n"); //uncomment if input is not from file
+	scanf("%d",&S);
+	//printf("Enter lines per set\n");
+	scanf("%d",&E);
+	//printf("Enter bytes per cache block\n");
+	scanf("%d",&B);
+	//printf("Enter address size\n");
+	scanf("%d",&m);
+	//printf("Enter replacement policy as LFU or LRU\n");
+	scanf("%s",policy);
+	//printf("Enter the hitTime\n");
+	scanf("%d",&hitTime);
+	//printf("Enter the miss penalty\n");
+	scanf("%d",&missPenalty);
+	printf("\n");
+
+
+	cache = malloc(sizeof(line*) * S);
+
+	for (int i = 0; i < S; i++){
+		cache[i] = malloc(sizeof(line)*E);
+	}
+
+	initialize_cache(S,E);
+	int b = log2(B);
+	int s = log2(S);
+	int tagsize = m - (s + b);
+
+	*ptag_mask = ~0 << (s + b);
+	*pset_mask = ~(zero_comp << s) << b;
+
+	int hexSize = log2(m);
+	char hex[9];
+	long address;
+	double misses = 0;
+	double accesses = 0;
+
+	while(1){
+		//printf("Enter a address");
+		scanf("%lx",&address);
+		//sscanf(hex,"%lx",&address);
+		if (address == -1){
+			break;
+		}
+		char hit = 0;
+		int tmp;
+		long setIndex = (address & set_mask) >> b;
+		for (int i = 0; i < E; i++){
+			if (check_hit(address, &cache[setIndex][i])){
+				hit = 1;
+				tmp = i;
+				break;
+			} 
+		}
+		if (hit){
+			printf("%lx  H\n",address);
+			*phitRate += hitTime;
+			inc_age(S,E,setIndex,tmp);
+			accesses++;
+		} else {
+			printf("%lx  M\n",address);
+			int line = replace(policy,setIndex,E);
+			cache[setIndex][line].valid = 1;
+			cache[setIndex][line].tag = (address & tag_mask);
+			*pmissPen += missPenalty + hitTime;
+			misses++;
+			accesses++;
+		}
+
+	}
+
+	/*for (int i = 0; i < S; i++){
+		for (int j = 0; j < E; j++){
+			printf("Set %d Line %d \nTag %ld valid %c counter %d age %d id %d\n",i,j,cache[i][j].tag,cache[i][j].valid,cache[i][j].counter,cache[i][j].age,cache[i][j].id);
+		}
+	}*/
+	double missRate = (misses/accesses) * 100;
+	int clockCycles = missPen + hitRate;
+
+	printf("Miss rate: %f Clock Cycles %d\n",missRate,clockCycles);
+
+	for (int i = 0; i < S; i++){
+		free(cache[i]);
+	}
+	free(cache);
+
+	return 0;
+}
+
+
+
+
+
